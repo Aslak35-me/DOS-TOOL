37b1347f36ca4f21f75e8281e6c3da1dd14f33f6
Aslak35-me <aslak35@proton.me>
2025-03-03 22:43:11 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ echo "==== DOS TOOL KURULUMU BAŞLIYOR ===="
 # Python bağımlılıklarını yükle
 sudo apt update -y
 sudo apt install -y python3 python3-pip
-pip3 install requests scapy
+pip3 install requests scapy colorama
 
 # Çalıştırma izinlerini ayarla
-chmod +x dos_menu.py mc_dos.py http_flood.py argparse_dos.py security_test.py
+chmod +x dos_menu.py mc_dos.py http_flood.py argparse_dos.py firewall_test.py
 
 echo "==== KURULUM TAMAMLANDI ===="
 echo "DOS Araçlarını çalıştırmak için: python3 dos_menu.py"
